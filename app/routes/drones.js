61179122bfe6ac449752a22a885df2b1cba9d1e0
bsantero <brian.santero@gmail.com>
2015-04-02 22:26:38 -0500

    Add route actions for Drones and Metronome

--- app/routes/drones.js
+++ app/routes/drones.js
@@ -1,4 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	actions: {
+		play: function() {
+			alert("Play pressed!");
+		},
+		stop: function() {
+			alert("Stop pressed!");
+		},
+		setDefault: function() {
+			alert("Default pressed!");
+		},
+		resetToDefault: function() {
+			alert("Reset pressed!");
+		},
+		changePitchUp: function() {
+			alert("Pitch going up!")
+		},
+		changePitchDown: function() {
+			alert("Pitch going down!");
+		}
+	}
 });
--- app/routes/metronome.js
+++ app/routes/metronome.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	actions: {
+		play: function() {
+			alert("Play pressed!");
+		},
+		stop: function() {
+			alert("Stop pressed!");
+		},
+		changeTempo: function(amount) {
+			alert("Tempo changing by "+ amount +"!");
+		}
+	}
 });
