a52304ea4256d4eda10656e4d4609bbe83ca8fac
bsantero <brian.santero@gmail.com>
2015-04-03 17:53:47 -0500

    Add pitch model and some actions

--- app/controllers/drones.js
+++ app/controllers/drones.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+	currentPitch: function() {
+		var array = this.get('model');
+		return array[this.get('arrayPos')];
+	},
+	controlDrone: function(controller) {
+		console.log('Playing status is now ' + controller.isPlaying +".");
+	}.observes('isPlaying')
 });
--- app/routes/drones.js
+++ app/routes/drones.js
@@ -3,31 +3,93 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 	setupController: function(controller){
 		controller.set('isPlaying', false);
-		controller.set('notes', []);
-		controller.set('currentPitchName', 'A');
-		controller.set('currentPitchOctave', 4);
-		controller.set('currentPitchFrequency', 440);
-		controller.set('noteRangeMin', 'A1');
-		controller.set('noteRangeMin', 'A6');
+		controller.set('arrayPos', 36);
+	},
+	model: function() {
+		return [
+			{ name: 'A', octave: 1, frequency: 55.0 },
+			{ name: 'A#', octave: 1, frequency: 58.27 },
+			{ name: 'B', octave: 1, frequency: 61.74 },
+			{ name: 'C', octave: 2, frequency: 65.41 },
+			{ name: 'C#', octave: 2, frequency: 69.30 },
+			{ name: 'D', octave: 2, frequency: 73.42 },
+			{ name: 'D#', octave: 2, frequency: 77.78 },
+			{ name: 'E', octave: 2, frequency: 82.41 },
+			{ name: 'F', octave: 2, frequency: 87.31 },
+			{ name: 'F#', octave: 2, frequency: 92.50 },
+			{ name: 'G', octave: 2, frequency: 98.00 },
+			{ name: 'G#', octave: 2, frequency: 103.83 },
+			{ name: 'A', octave: 2, frequency: 110.0 },
+			{ name: 'A#', octave: 2, frequency: 116.54 },
+			{ name: 'B', octave: 2, frequency: 123.47 },
+			{ name: 'C', octave: 3, frequency: 130.81 },
+			{ name: 'C#', octave: 3, frequency: 138.59 },
+			{ name: 'D', octave: 3, frequency: 146.43 },
+			{ name: 'D#', octave: 3, frequency: 155.56 },
+			{ name: 'E', octave: 3, frequency: 164.81 },
+			{ name: 'F', octave: 3, frequency: 174.61 },
+			{ name: 'F#', octave: 3, frequency: 185.00 },
+			{ name: 'G', octave: 3, frequency: 196.00 },
+			{ name: 'G#', octave: 3, frequency: 207.65 },
+			{ name: 'A', octave: 3, frequency: 220.00 },
+			{ name: 'A#', octave: 3, frequency: 233.08 },
+			{ name: 'B', octave: 3, frequency: 246.94 },
+			{ name: 'C', octave: 4, frequency: 261.63 },
+			{ name: 'C#', octave: 4, frequency: 277.18 },
+			{ name: 'D', octave: 4, frequency: 293.66 },
+			{ name: 'D#', octave: 4, frequency: 311.13 },
+			{ name: 'E', octave: 4, frequency: 329.63 },
+			{ name: 'F', octave: 4, frequency: 349.23 },
+			{ name: 'F#', octave: 4, frequency: 369.99 },
+			{ name: 'G', octave: 4, frequency: 392.00 },
+			{ name: 'G#', octave: 4, frequency: 415.30 },
+			{ name: 'A', octave: 4, frequency: 440.00 },
+			{ name: 'A#', octave: 4, frequency: 466.16 },
+			{ name: 'B', octave: 4, frequency: 493.88 },
+			{ name: 'C', octave: 5, frequency: 523.25 },
+			{ name: 'C#', octave: 5, frequency: 554.37 },
+			{ name: 'D', octave: 5, frequency: 587.33 },
+			{ name: 'D#', octave: 5, frequency: 622.25 },
+			{ name: 'E', octave: 5, frequency: 659.25 },
+			{ name: 'F', octave: 5, frequency: 698.46 },
+			{ name: 'F#', octave: 5, frequency: 739.99 },
+			{ name: 'G', octave: 5, frequency: 783.99 },
+			{ name: 'G#', octave: 5, frequency: 830.61 },
+			{ name: 'A', octave: 5, frequency: 880.00 },
+			{ name: 'A#', octave: 5, frequency: 932.33 },
+			{ name: 'B', octave: 5, frequency: 987.77 },
+			{ name: 'C', octave: 6, frequency: 1046.50 },
+			{ name: 'C#', octave: 6, frequency: 1108.73 },
+			{ name: 'D', octave: 6, frequency: 1174.66 },
+			{ name: 'D#', octave: 6, frequency: 1244.51 },
+			{ name: 'E', octave: 6, frequency: 1318.51 },
+			{ name: 'F', octave: 6, frequency: 1396.91 },
+			{ name: 'F#', octave: 6, frequency: 1479.98 },
+			{ name: 'G', octave: 6, frequency: 1567.98 },
+			{ name: 'G#', octave: 6, frequency: 1661.22 },
+			{ name: 'A', octave: 6, frequency: 1760.00 }
+		];
 	},
 	actions: {
 		play: function() {
-			alert(currentPitchName);
+			this.controller.set('isPlaying', true);
 		},
 		stop: function() {
-			alert("Stop pressed!");
+			this.controller.set('isPlaying', false);
 		},
 		setDefault: function() {
 			alert("Default pressed!");
 		},
 		resetToDefault: function() {
-			alert("Reset pressed!");
+			alert(this.controller.isPlaying);
 		},
 		changePitchUp: function() {
-			alert("Pitch going up!")
+			// var newPos = this.get('arrayPos') + 1;
+			// this.controller.set('arrayPos', newPos;
 		},
 		changePitchDown: function() {
-			alert("Pitch going down!");
+			// var newPos = this.get('arrayPos') - 1;
+			// this.controller.set('arrayPos', newPos;
 		}
 	}
 });
