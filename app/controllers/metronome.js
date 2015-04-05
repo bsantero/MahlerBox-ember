b761775f0837ad511dc23955cc5f7a9527fb337b
bsantero <brian.santero@gmail.com>
2015-04-05 17:44:49 -0500

    Wire up main Metronome app buttons

--- app/controllers/metronome.js
+++ app/controllers/metronome.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  isPlaying: false,
+  currentTempo: 120,
+  actions: {
+    play: function() {
+      this.set('isPlaying', true);
+    },
+    stop: function() {
+      this.set('isPlaying', false);
+    },
+		changeTempo: function(amount) {
+      let tempo = this.get('currentTempo') + amount;
+      if (tempo >= 40 && tempo <= 240) {
+		    this.set('currentTempo', tempo);
+      }
+		}
+  }
 });
--- app/routes/metronome.js
+++ app/routes/metronome.js
@@ -6,16 +6,5 @@ export default Ember.Route.extend({
 		this.controllerFor('application').set('dronesActive', false);
 		this.controllerFor('application').set('infoMenuOpen', false);
 		this.controllerFor('application').set('settingsMenuOpen', false);
-	},
-	actions: {
-		play: function() {
-			alert("Play pressed!");
-		},
-		stop: function() {
-			alert("Stop pressed!");
-		},
-		changeTempo: function(amount) {
-			alert("Tempo changing by "+ amount +"!");
-		}
 	}
 });
